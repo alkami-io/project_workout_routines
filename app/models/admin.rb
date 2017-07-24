23c80793fa21a57ae81913b67654dbfd159d4381
Robert Cornell <robert@alkami.io>
2017-07-23 23:25:06 -0600

    added devise and wired up.

---
+++ app/models/admin.rb
@@ -0,0 +1,6 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable
+end
